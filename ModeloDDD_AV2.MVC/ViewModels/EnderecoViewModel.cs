efbfd1adcf8c697d44caff0300bd61f823f76057
verteingdv <vertein.dev@gmail.com>
2016-12-06 22:37:22 -0200

    Update EnderecoViewModel.cs
    Adicionado o limite máximo de 100 caracteres para o atributo Logradouro.
    Adicionado o limite máximo de 15 caracteres para o atributo Número.
    Adicionado o limite máximo de 100 caracteres para o atributo complemento.
    Adicionado o limite máximo de 100 caracteres para o atributo Bairro.
    Adicionado o limite máximo de 100 caracteres para o atributo Município.
    Adicionado o limite máximo de 9 caracteres para o atributo Logradouro.
    Adicionado Expressão Regular para o atributo cep para somente aceitar o formato 00000-000.

--- ModeloDDD_AV2.MVC/ViewModels/EnderecoViewModel.cs
+++ ModeloDDD_AV2.MVC/ViewModels/EnderecoViewModel.cs
@@ -12,24 +12,34 @@ namespace ModeloDDD_AV2.MVC.ViewModels
         public int EnderecoId { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Logradouro.")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres.")]
         public string Logradouro { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Numero.")]
+        [MaxLength(15, ErrorMessage = "Máximo de {0} caracteres.")]
         public string Numero { get; set; }
 
+        
         [Required(ErrorMessage = "Preencha o campo Complemento.")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres.")]
         public string Complemento { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Bairro.")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres.")]
         public string Bairro { get; set; }
 
+        
         [Required(ErrorMessage = "Preencha o campo Municipio.")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres.")]
         public string Municipio { get; set; }
 
-        [Required(ErrorMessage = "Preencha o campo Cep.")]
+        [Required(ErrorMessage = "Preencha o campo CEP.")]
+        [MaxLength(9, ErrorMessage = "Máximo de {0} caracteres.")]
+        [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "O CEP deverá estar no formato 00000-000")]
         public string Cep { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Uf.")]
+        [MaxLength(2, ErrorMessage = "Máximo de {0} caracteres. Exemplo: RJ para Rio de Janeiro, SP para São Paulo...")]
         public string Uf { get; set; }
 
         public int FornecedorId { get; set; }
